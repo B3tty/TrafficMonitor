de6f7afd2687d5a50973a6eb707091e18d74ab2c
Betty Moreschini <bmoreschini@vente-privee.com>
2020-02-05 18:41:48 +0100

    update log to account for identity

--- TrafficMonitor.Tests/LogTest.cs
+++ TrafficMonitor.Tests/LogTest.cs
@@ -15,15 +15,46 @@ namespace TrafficMonitor.Tests
             Log logParsed = new Log("127.0.0.1 - betty [09/May/2018:16:00:42 +0000] \"POST /api/user HTTP/1.0\" 503 12");
             Log logExpected = new Log{
                 ip="127.0.0.1",
+                ident="-",
                 user="betty",
-                //timestamp=DateTime.ParseExact("09/May/2018:16:00:42 +0000", "dd/MMM/yyyy:H:mm:ss zzz", System.Globalization.CultureInfo.InvariantCulture),
                 timestamp=new DateTime(2018, 05, 09, 16, 0, 42, DateTimeKind.Utc),
-                resource="POST /api/user HTTP/1.0",
+                request="POST /api/user HTTP/1.0",
                 section="api",
                 responseCode=503,
                 responseSize=12
             };
             Assert.Equal(logExpected.timestamp, logParsed.timestamp);
+        }
+
+        [Fact]
+        public void Ctor_SectionParsing_LotsOfSections()
+        {
+            Log logParsed = new Log("127.0.0.1 - betty [09/May/2018:16:00:42 +0000] \"POST /a/b/c/d HTTP/1.0\" 503 12");
+            Log logExpected = new Log{section="a"};
+            Assert.Equal(logExpected.section, logParsed.section);
+        }
+
+        [Fact]
+        public void Ctor_SectionParsing_NoSubSection()
+        {
+            Log logParsed = new Log("127.0.0.1 - betty [09/May/2018:16:00:42 +0000] \"POST /a HTTP/1.0\" 503 12");
+            Log logExpected = new Log{section="a"};
+            Assert.Equal(logExpected.section, logParsed.section);
+        }
+
+        [Fact]
+        public void Ctor_SectionParsing_NoSection()
+        {
+            // Shouldn't happen, we just don't want it to crash
+            Log logParsed = new Log("127.0.0.1 - betty [09/May/2018:16:00:42 +0000] \"POST HTTP/1.0\" 503 12");
+        }
+
+        [Fact]
+        public void Ctor_SectionParsing_IncompleteResource()
+        {
+            Log logParsed = new Log("127.0.0.1 - betty [09/May/2018:16:00:42 +0000] \"POST\" 503 12");
+            Log logExpected = new Log{section=""};
+            Assert.Equal(logExpected.section, logParsed.section);
         }
     }
 }
--- TrafficMonitor/Log.cs
+++ TrafficMonitor/Log.cs
@@ -8,9 +8,10 @@ namespace TrafficMonitor
     public class Log
     {
         public String ip;
+        public String ident;
         public String user;
         public DateTime timestamp;
-        public String resource;
+        public String request;
         public String section;
         public int responseCode;
         public int responseSize;
@@ -21,22 +22,23 @@ namespace TrafficMonitor
         {
             // 127.0.0.1 - betty [09/May/2018:16:00:43 +0000] "GET /report HTTP/1.0" 200 124
             // ip - user [timestamp] "resource" responseCode responseSize
-            var r = new Regex(@"([0-9.]+)\s-\s(.+)\s\[(.+)\]\s""(.+)""\s([0-9]+)\s([0-9]+)", RegexOptions.IgnoreCase);
+            var r = new Regex(@"([0-9.\-]+)\s([a-zA-Z\-]+)\s(.+)\s\[(.+)\]\s""(.+)""\s([0-9\-]+)\s([0-9\-]+)", RegexOptions.IgnoreCase);
             var match = r.Match(line);
             if(match.Success)
             {
                 ip = match.Groups[1].Value;
-                user = match.Groups[2].Value;
+                ident = match.Groups[2].Value;
+                user = match.Groups[3].Value;
                 timestamp = DateTime.ParseExact(
-                    match.Groups[3].Value,
+                    match.Groups[4].Value,
                     "dd/MMM/yyyy:H:mm:ss zzz",
                     System.Globalization.CultureInfo.InvariantCulture,
                     System.Globalization.DateTimeStyles.AdjustToUniversal);
-                resource = match.Groups[4].Value;
-                responseCode = int.Parse(match.Groups[5].Value);
-                responseSize = int.Parse(match.Groups[6].Value);
+                request = match.Groups[5].Value;
+                responseCode = int.Parse(match.Groups[6].Value);
+                responseSize = int.Parse(match.Groups[7].Value);
 
-                String[] sections = resource.Split(' ');
+                String[] sections = request.Split(' ');
                 section = sections.Count()>1 ? sections[1] : "";
                 sections = section.Split('/');
                 section = sections.Count()>1 ? sections[1] : "";
@@ -50,7 +52,7 @@ namespace TrafficMonitor
                 && ip == log.ip
                 && user == log.user
                 && timestamp == log.timestamp
-                && resource == log.resource
+                && request == log.request
                 && section == log.section
                 && responseCode == log.responseCode
                 && responseSize == log.responseSize;
